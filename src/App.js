bae91355553e78f4e9e950425318a8fcc959a658
BrunoFgR <brunofds281@gmail.com>
2020-05-02 20:19:41 -0300

    feat: Creando novos repositorios

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import "./styles.css";
+import api from './services/api';
+
+let count = 0
 
 function App() {
+  const [diretorios, setDiretorios] = useState([]);
+
+  useEffect(() => {
+    async function loadingRepository() {
+      const response = await api.get('/repositories');
+
+      setDiretorios(response.data)
+    }
+
+    loadingRepository()
+  }, [])
+
+
+
   async function handleAddRepository() {
-    // TODO
+    count = count + 1;
+
+    const data = {
+      title: `Repositorio ${count}`,
+      url: `https://github.com/BrunoFgR/Modulo${count}`,
+      techs: ["nodeJS", "vueJS", "expressJS"]
+    }
+
+    const response = await api.post('/repositories', data);
+
+    setDiretorios([ ...diretorios, response.data ])
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`/repositories/${id}`);
+
+    const newDiretorio = diretorios.filter(d => d.id !== id);
+    setDiretorios(newDiretorio)
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {diretorios.map(d => (
+            <li key={d.id}>
+              {d.title}
+  
+            <button onClick={() => handleRemoveRepository(d.id)}>
+              Remover
+            </button>
+            </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
